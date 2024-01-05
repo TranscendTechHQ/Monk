8956809e3c9b06c8ef2696c10bff443c72ecb73d
yksoni-transcendtech <126711642+yksoni-transcendtech@users.noreply.github.com>
2024-01-04 22:33:37 -0800

    added user management dashboard via awesome supertokens. available at http://localhost:8000/auth/dashboard

--- backend/fastapi/config/__init__.py
+++ backend/fastapi/config/__init__.py
@@ -24,3 +24,33 @@ class Settings(CommonSettings, ServerSettings, DatabaseSettings):
 
 
 settings = Settings()
+
+
+
+from supertokens_python import init, InputAppInfo, SupertokensConfig
+from supertokens_python.recipe import emailpassword, session
+from supertokens_python.recipe import dashboard
+
+init(
+    app_info=InputAppInfo(
+        app_name="Monk",
+        api_domain="http://localhost:8000",
+        website_domain="http://localhost:8000",
+        api_base_path="/auth",
+        website_base_path="/auth"
+    ),
+    supertokens_config=SupertokensConfig(
+        # https://try.supertokens.com is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.
+        #connection_uri="https://try.supertokens.com",
+        connection_uri="http://localhost:3567/",
+        #connection_uri="http://supertokens:3567",
+        # api_key=<API_KEY(if configured)>
+    ),
+    framework='fastapi',
+    recipe_list=[
+        session.init(), # initializes session features
+        emailpassword.init(),
+        dashboard.init(),
+    ],
+    mode='asgi' # use wsgi if you are running using gunicorn
+)
--- backend/fastapi/main.py
+++ backend/fastapi/main.py
@@ -1,4 +1,8 @@
+from supertokens_python import get_all_cors_headers
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from supertokens_python.framework.fastapi import get_middleware
+
 import uvicorn
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import settings
@@ -16,6 +20,20 @@ async def lifespan(app: FastAPI):
     await shutdown_db_client()
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(get_middleware())
+
+# TODO: Add APIs
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:8000",
+    ],
+    allow_credentials=True,
+    allow_methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
+    allow_headers=["Content-Type"] + get_all_cors_headers(),
+)
 
 
 
