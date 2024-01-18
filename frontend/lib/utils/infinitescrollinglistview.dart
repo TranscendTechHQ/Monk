7d8f02c2b627c850136177a9726a50688b55fbd6
yksoni-transcendtech <126711642+yksoni-transcendtech@users.noreply.github.com>
2024-01-17 18:01:27 -0800

    added infinitescrollinglistview util. Need to test

---
+++ frontend/lib/utils/infinitescrollinglistview.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class InfiniteScrollingListView extends StatefulWidget {
+  @override
+  _InfiniteScrollingListViewState createState() =>
+      _InfiniteScrollingListViewState();
+}
+
+class _InfiniteScrollingListViewState extends State<InfiniteScrollingListView> {
+  ScrollController _scrollController = ScrollController();
+  List<int> _dataList = List.generate(20, (index) => index);
+
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollListener() {
+    if (_scrollController.position.pixels ==
+        _scrollController.position.maxScrollExtent) {
+      _loadMoreData();
+    }
+  }
+
+  Future<void> _loadMoreData() async {
+    if (!_isLoading) {
+      setState(() {
+        _isLoading = true;
+      });
+
+      // Simulating a delay to fetch more data
+      await Future.delayed(Duration(seconds: 2));
+
+      List<int> newDataList =
+          List.generate(20, (index) => index + _dataList.length);
+
+      setState(() {
+        _dataList.addAll(newDataList);
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      controller: _scrollController,
+      itemCount: _dataList.length + 1,
+      itemBuilder: (context, index) {
+        if (index < _dataList.length) {
+          return ListTile(
+            title: Text('Item ${_dataList[index]}'),
+          );
+        } else {
+          return _isLoading
+              ? Center(child: CircularProgressIndicator())
+              : Container();
+        }
+      },
+    );
+  }
+}
