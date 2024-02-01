63ace3f934358d71b263ce1da93f0e8d138f6ee2
yksoni-transcendtech <126711642+yksoni-transcendtech@users.noreply.github.com>
2024-01-31 16:44:50 -0800

    fixed auto-generated documentation for threads endpoint

--- backend/api/routes/threads/routers.py
+++ backend/api/routes/threads/routers.py
@@ -15,7 +15,7 @@ router = APIRouter()
 
 @router.post("/threads", response_model=ThreadModel)
 async def create_thread(request: Request, thread_data: CreateThreadModel = Body(...), 
-                        session: SessionContainer = Depends(verify_session)):
+                        session: SessionContainer = Depends(verify_session())):
     # Create a new thread in MongoDB using the thread_data
     # Index the thread by userId
     
@@ -31,7 +31,7 @@ async def create_thread(request: Request, thread_data: CreateThreadModel = Body(
 
 @router.put("/threads/{title}", response_model=ThreadModel)
 async def update_thread(request: Request, title: str, thread_data: UpdateThreadModel, 
-                        session: SessionContainer = Depends(verify_session)):
+                        session: SessionContainer = Depends(verify_session())):
     # Update an existing thread in MongoDB identified by the title
     thread = jsonable_encoder(thread_data)
     
@@ -44,7 +44,7 @@ async def update_thread(request: Request, title: str, thread_data: UpdateThreadM
 
 @router.get("/threads/{title}", response_model=ThreadModel)
 async def get_thread(request: Request, title: str, 
-                     session: SessionContainer = Depends(verify_session)):
+                     session: SessionContainer = Depends(verify_session())):
     # Get a thread from MongoDB by title
 
     old_thread = await get_mongo_document({"title": title}, request.app.mongodb["threads"])
@@ -54,7 +54,7 @@ async def get_thread(request: Request, title: str,
                        content=jsonable_encoder(old_thread))
 
 @router.get("/threads", response_model=List[ThreadsModel])
-async def get_all_threads(request: Request, session: SessionContainer = Depends(verify_session)):
+async def get_all_threads(request: Request, session: SessionContainer = Depends(verify_session())):
     # Get all threads from MongoDB by date created
     threads = await get_mongo_documents(request.app.mongodb["threads"])
     return threads
