4bc267fc85b251cb53a7d8f287b27a4f36e3a05f
Sonu Sharma <sonu.sharma045@gmail.com>
2024-03-26 21:48:11 +0530

    Add flutter_appauth google implementation

--- frontend/lib/ui/pages/login_page.dart
+++ frontend/lib/ui/pages/login_page.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/widgets.dart' as prefix;
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:frontend/helper/constants.dart';
+import 'package:flutter/widgets.dart' as prefix;
+import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:frontend/main.dart';
 import 'package:frontend/ui/pages/home_page.dart';
 //import 'package:flutter/services.dart';
@@ -21,12 +21,11 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  FlutterAppAuth appAuth = FlutterAppAuth();
   Future<void> loginWithGoogle() async {
     GoogleSignIn googleSignIn;
 
     googleSignIn = GoogleSignIn(
-      clientId: Constants.googleClientId,
-      serverClientId: Constants.backendGoogleClientId,
       scopes: [
         'email',
       ],
@@ -96,6 +95,71 @@ class _LoginPageState extends State<LoginPage> {
               onPressed: () {
                 loginWithGoogle();
               },
+            ),
+            // Slack login
+            IconButton(
+              onPressed: () async {
+                // final result2 = await appAuth.authorizeAndExchangeCode(
+                //   AuthorizationTokenRequest(
+                //     '4287010774055.6810855127681',
+                //     'https://a9ed-2405-201-6817-6087-20d9-7106-a3ac-f09d.ngrok-free.app/api/auth/calllback/slack',
+                //     clientSecret: '34d290bec1981741d0f332d55e81c875',
+                //     discoveryUrl:
+                //         'https://slack.com/.well-known/openid-configuration',
+                //     serviceConfiguration:
+                //         const AuthorizationServiceConfiguration(
+                //       authorizationEndpoint:
+                //           "https://slack.com/openid/connect/authorize",
+                //       tokenEndpoint:
+                //           "https://slack.com/api/openid.connect.token",
+                //       endSessionEndpoint:
+                //           'https://oauth2.googleapis.com/revoke',
+                //     ),
+                //     scopes: ['openid', 'profile', 'email'],
+                //   ),
+                // );
+
+                final result1 = await appAuth.authorizeAndExchangeCode(
+                  AuthorizationTokenRequest(
+                    '337392647778-3j84aqtmia13h4rnn76ud66q2aacjr56.apps.googleusercontent.com',
+                    'app.heymonk:/oauthredirect',
+                    serviceConfiguration:
+                        const AuthorizationServiceConfiguration(
+                      authorizationEndpoint:
+                          'https://accounts.google.com/o/oauth2/v2/auth',
+                      tokenEndpoint: 'https://oauth2.googleapis.com/token',
+                      endSessionEndpoint:
+                          'https://oauth2.googleapis.com/revoke',
+                    ),
+                    scopes: ['email', 'openid', 'profile'],
+                  ),
+                );
+                // print(result1?.);
+                var result = await NetworkManager.instance.client.post(
+                  "/auth/signinup",
+                  data: {
+                    "thirdPartyId": "google",
+                    "oAuthTokens": {
+                      "access_token": result1?.accessToken,
+                      "id_token": result1?.idToken
+                    },
+                  },
+                  options: Options(
+                    headers: {'rid': 'thirdpartypasswordless'},
+                  ),
+                );
+                loader.hideLoader();
+                if (result.statusCode == 200) {
+                  Future.delayed(
+                    Duration.zero,
+                    () {
+                      prefix.Navigator.of(context)
+                          .pushReplacementNamed(HomePage.route);
+                    },
+                  );
+                }
+              },
+              icon: const Icon(Icons.login),
             ),
           ],
         ),
