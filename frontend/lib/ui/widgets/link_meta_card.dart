2d71d6357c2bfc768ddc97b71e34d29f2675f0a4
Sonu Sharma <sonu.sharma045@gmail.com>
2024-05-16 12:07:39 +0530

    Make links in block clickable.

--- frontend/lib/ui/widgets/link_meta_card.dart
+++ frontend/lib/ui/widgets/link_meta_card.dart
@@ -1,7 +1,9 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:frontend/ui/theme/theme.dart';
 import 'package:frontend/ui/widgets/cache_image.dart';
 import 'package:openapi/openapi.dart';
+
+import '../../helper/utils.dart';
 
 class LinkMetaCard extends StatelessWidget {
   const LinkMetaCard({super.key, required this.linkMeta});
@@ -12,48 +14,56 @@ class LinkMetaCard extends StatelessWidget {
     if (linkMeta == null || linkMeta!.url.isNullOrEmpty) {
       return const SizedBox();
     }
-    return Container(
-      constraints: const BoxConstraints(maxHeight: 300, minHeight: 100),
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: context.colorScheme.surface,
-        borderRadius: BorderRadius.circular(8),
-        border:
-            Border.all(color: context.colorScheme.onSurface.withOpacity(0.1)),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          if (linkMeta!.title != null)
-            Text(
-              linkMeta!.title!,
-              style: context.textTheme.bodyMedium,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            ),
-          if (linkMeta!.description != null) ...[
-            const SizedBox(height: 8),
-            Text(
-              linkMeta!.description!,
-              style: context.textTheme.bodySmall!.copyWith(
-                color: context.colorScheme.onSurface.withOpacity(0.6),
+    return InkWell(
+      onTap: () {
+        launch(linkMeta!.url);
+      },
+      child: Container(
+        constraints: const BoxConstraints(maxHeight: 300, minHeight: 100),
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: context.colorScheme.surface,
+          borderRadius: BorderRadius.circular(8),
+          border:
+              Border.all(color: context.colorScheme.onSurface.withOpacity(0.1)),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (linkMeta!.title != null)
+              Text(
+                linkMeta!.title!,
+                style: context.textTheme.bodyMedium,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
               ),
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
+            if (linkMeta!.description != null) ...[
+              const SizedBox(height: 8),
+              Text(
+                linkMeta!.description!,
+                style: context.textTheme.bodySmall!.copyWith(
+                  color: context.colorScheme.onSurface.withOpacity(0.6),
+                ),
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ],
+            if (linkMeta!.image.isNotNullEmpty) ...[
+              const SizedBox(height: 8),
+              CacheImage(
+                onPressed: () {
+                  launch(linkMeta!.url);
+                },
+                path: linkMeta!.image!,
+                fit: BoxFit.fitHeight,
+                errorWidget: Container(
+                  height: 100,
+                  color: context.colorScheme.onSurface.withOpacity(0.1),
+                ),
+              ).extended
+            ]
           ],
-          if (linkMeta!.image.isNotNullEmpty) ...[
-            const SizedBox(height: 8),
-            CacheImage(
-              path: linkMeta!.image!,
-              fit: BoxFit.fitHeight,
-              errorWidget: Container(
-                height: 100,
-                color: context.colorScheme.onSurface.withOpacity(0.1),
-              ),
-            ).extended
-          ]
-        ],
+        ),
       ),
     );
   }
