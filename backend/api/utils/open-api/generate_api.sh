7a298f9d8204c7b64ff309e77db8eadde6141ff5
Yogesh Soni <126711642+yksoni-monk@users.noreply.github.com>
2025-02-26 11:45:57 -0800

    missing openapi generator installed. TODO find a tool that is actively maintained

--- backend/api/utils/open-api/generate_api.sh
+++ backend/api/utils/open-api/generate_api.sh
@@ -2,4 +2,4 @@
 FRONTEND_PATH="../../../../frontend/src/api"
 ./generate_yaml.sh
 rm -rf $FRONTEND_PATH
-openapi -i openapi.yaml -o $FRONTEND_PATH
+npx openapi-typescript-codegen -i openapi.yaml -o $FRONTEND_PATH
